1da140cb16a9492a4129ddf937c56c35323be485
Ángel Ortega <codenode.ortega@gmail.com>
2024-06-24 17:54:47 -0600

    refactor: Add custom scrollbar utility to tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,5 +4,17 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		function ({ addUtilities }){
+			addUtilities({
+				'.scrollbar-hide':{
+					'&::-webkit-scrollbar':{
+						display: 'none'
+					},
+					'-ms-overflow-style': 'none',
+					'scrollbar-width': 'none'
+				}
+			})
+		}
+	],
 }
