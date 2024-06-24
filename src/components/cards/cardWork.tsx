fd92b7fb91f1822387bc17ed512a39edbd1b8510
Ángel Ortega <codenode.ortega@gmail.com>
2024-06-24 12:47:20 -0600

    add searching of project

---
+++ src/components/cards/cardWork.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+interface CardWorkProps {
+  tags: string[];
+  id: string;
+  title: string;
+  body: string;
+  image: string;
+  href: string;
+}
+
+const CardWork: React.FC<CardWorkProps> = ({ tags, id, title, body, image, href }) => {
+  const orangeTwo = 'text-orange-500';
+
+
+  return (
+    <button key={id} className=" text-left rounded-lg px-5 py-2 border border-gray-500 border-spacing-5 max-sm:pb-20 max-sm:gap-8 gap-28">
+      <div className="max-sm:w-full">
+        <section className="flex justify-between w-full">
+          <h2
+            className={' max-sm:text-center text-2xl  font-semibold ' + orangeTwo}
+          >
+            <span className="text-gray-300">0{id} /</span>
+            {title}
+          </h2>
+          <svg
+            aria-hidden="true"
+            height="16"
+            viewBox="0 0 16 16"
+            version="1.1"
+            width="16"
+            data-view-component="true"
+            className="fill-current text-gray-300 max-sm:hidden h-6 w-6"
+          >
+            <path d="M10 13a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0-4a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm-4 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm5-9a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM7 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM6 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"></path>
+          </svg>
+        </section>
+        <div className="mb-5">
+          {tags &&
+            tags.map((tag, index )=> {
+              return <span key={index} className="text-gray-400 text-md mb-5"> {tag} /</span>;
+            })}
+        </div>
+        <p className="text-gray-300 mb-7">{body}</p>
+      </div>
+    </button>
+  );
+};
+
+export default CardWork;
+
---
+++ src/components/works.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import CardWork from './cards/cardWork';
+import data from './data/data.json';
+
+
+
+const Works = ({ type }) => {
+  const [projects, setProjects] = useState(data.projects);
+  const [filter, setFilter] = useState('');
+  const [filterTag, setFilterTag] = useState(1);  
+
+  useEffect(() => {
+    if (type === 2) {
+      setProjects(data.blog);
+    }
+  }, [type]);
+
+  const filteredProjects = projects.filter(project =>{    
+    if (filterTag === "All") {
+      return project.title.toLowerCase().includes(filter.toLowerCase());
+    }
+    if (filterTag !== 1) {
+      return project.tags.includes(filterTag);
+    } 
+    return project.title.toLowerCase().includes(filter.toLowerCase());
+  });
+
+  return (
+    <section className="my-16 w-full">
+      <div>
+        <h2 className="text-3xl font-bold text-center">
+          {type === 1 ? 'Projects' : 'Blog'}
+        </h2>
+        <p className="text-center text-gray-500 mb-2">
+          {type === 1
+            ? 'Here are some of the projects I have worked on.'
+            : 'Here are some of the blog posts I have written.'}
+        </p>
+        <div className="flex justify-center">
+          <input
+            id="search"
+            className="w-full max-w-96 bg-transparent border rounded-lg px-4"
+            type="text"
+            value={filter}
+            onChange={e => setFilter(e.target.value)}
+          />
+        </div>
+      </div>
+      <br />
+      <div className="bg-transparent w-full flex justify-end mb-3">
+        <select
+          id="carSelect"
+          className="px-2 text-center rounded-md bg-transparent"
+          value={filterTag}
+          onChange={e => setFilterTag(e.target.value)}
+        >
+          <option value="All">All</option>
+          <option value="React">React</option>
+          <option value="Java">Java</option>
+          <option value=".NET">.NET</option>
+          <option value="Typescript">Typescript</option>
+          <option value="Python">Python</option>
+        </select>
+      </div>
+      <section id="projects" className="grid grid-cols-2 gap-5">
+        {filteredProjects.map((item, index) => {
+          let i = index + 1;
+          return (
+            <CardWork
+              id={i}
+              title={item.title}
+              body={item.description}
+              image={item.image}
+              url={item.url}
+              tags={item.tags}
+            />
+          );
+        })}
+        {filteredProjects.length === 0 && (
+          <p className="w-[960px] max-w-6xl text-center text-gray-500">
+            No projects found with the name "{filter}".
+          </p>
+        )}
+      </section>
+    </section>
+  );
+};
+
+export default Works;
