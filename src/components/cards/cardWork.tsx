eaa27a64c4474621c8183717ad370a76a2d35760
aomerge <codenode.ortega@gmail.com>
2024-09-03 11:07:14 -0600

    refactor: Update CardWork component with improved styles and accessibility

--- src/components/cards/cardWork.tsx
+++ src/components/cards/cardWork.tsx
@@ -17,7 +17,7 @@ const CardWork: React.FC<CardWorkProps> = ({
   image,
   url,
 }) => {
-  const orangeTwo = 'text-orange-500';
+  const orangeTwo = 'text-blue-800';
   const handleClick = () => {
     window.open(url, '_blank');
   };
@@ -26,16 +26,16 @@ const CardWork: React.FC<CardWorkProps> = ({
     <button
       onClick={handleClick}
       key={id}
-      className=" text-left rounded-lg px-5 py-2 border border-gray-500 border-spacing-5 max-sm:pb-20 max-sm:gap-8 gap-28"
+      className=" shadow shadow-blue-300 text-left rounded-lg px-5 py-2 border border-[#bdddfa] hover:bg-[#bdddfa] border-spacing-5 max-sm:pb-20 max-sm:gap-8 gap-28"
     >
       <div className="max-sm:w-full">
         <section className="flex justify-between w-full">
           <h2
             className={
-              ' max-sm:text-center text-2xl  font-semibold ' + orangeTwo
+              ' max-sm:text-center text-2xl  font-bold ' + orangeTwo
             }
           >
-            <span className="text-gray-300">0{id} /</span>
+            <span className="text-blue-800">0{id} /</span>
             {title}
           </h2>
           <svg
@@ -45,7 +45,7 @@ const CardWork: React.FC<CardWorkProps> = ({
             version="1.1"
             width="16"
             data-view-component="true"
-            className="fill-current text-gray-300 max-sm:hidden h-6 w-6"
+            className="fill-current text-blue-500 max-sm:hidden h-6 w-6"
           >
             <path d="M10 13a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm0-4a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm-4 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm5-9a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM7 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM6 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"></path>
           </svg>
@@ -54,14 +54,14 @@ const CardWork: React.FC<CardWorkProps> = ({
           {tags &&
             tags.map((tag, index) => {
               return (
-                <span key={index} className="text-gray-400 text-md mb-5">
+                <span key={index} className="text-blue-800 font-light text-md mb-5">
                   {' '}
                   {tag} /
                 </span>
               );
             })}
         </div>
-        <p className="text-gray-300 mb-7">{body}</p>
+        <p className="text-blue-950 font-light mb-7">{body}</p>
       </div>
     </button>
   );
