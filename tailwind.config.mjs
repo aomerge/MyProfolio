d2274385c9959e059ff725562aa15dd9220841f7
aomerge <codenode.ortega@gmail.com>
2024-09-03 11:06:56 -0600

    refactor: Update tailwind.config.mjs with new blue color palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,33 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				'blue-100':'#e1eefd',
+				'blue-200':'#bdddfa',
+				'blue-300':'#82c1f7',
+				'blue-400':'#40a3f0',				
+				'blue-500': '#1787e0',
+				'blue-600': '#0960ae',
+				'blue-700': '#09549b',
+				'blue-800': '#0c4880',
+				'blue-900': '#103d6a',
+				'blue-950': '#0b2646'
+			},
+			textColor: {
+				'blue-100':'#e1eefd',
+				'blue-200':'#bdddfa',
+				'blue-300':'#82c1f7',
+				'blue-400':'#40a3f0',				
+				'blue-500': '#1787e0',
+				'blue-600': '#0960ae',
+				'blue-700': '#09549b',
+				'blue-800': '#0c4880',
+				'blue-900': '#103d6a',
+				'blue-950': '#0b2646'
+			}			
+
+		},
 	},
 	plugins: [
 		function ({ addUtilities }){
